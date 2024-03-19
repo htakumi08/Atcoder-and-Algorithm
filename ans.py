7d1bc3428e055f520392846a26b5690adc7981cb
h_takumi <takumi2285tkm@gmail.com>
2024-03-19 12:12:39 +0900

    atcodr commit

---
+++ ans.py
@@ -0,0 +1,18 @@
+import numpy as np
+import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
+import copy, math, bisect
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+# 存在しないkeyアクセス時デフォルト値生成
+from collections import defaultdict
+# リスト各要素の数え上げ 辞書型サブクラス
+from collections import Counter
+import io,sys,os
+os.system("clear")
+with open("HandInput.txt") as TxtOpen:
+    INPUT=TxtOpen.read() 
+sys.stdin=io.StringIO(INPUT)
+# --------------------------------------------------------
